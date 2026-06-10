c4843f49e43130a18405487b1141b41033c4f55a
Kavyarai-11a <kavyarai812@gmail.com>
2026-06-10 22:02:55 +0530

    Mini_project

--- mini.c
+++ mini.c
@@ -56,3 +56,22 @@ void drawRectangle(int x1,int y1,int x2,int y2) {
     drawLine(x2,y2,x1,y2);
     drawLine(x1,y2,x1,y1);
 }
+void drawCircle(int cx,int cy,int r) {
+    int x=r,y=0,p=1-r;
+    while(x>=y) {
+        setPixel(cx+x,cy+y); setPixel(cx-x,cy+y);
+        setPixel(cx+x,cy-y); setPixel(cx-x,cy-y);
+        setPixel(cx+y,cy+x); setPixel(cx-y,cy+x);
+        setPixel(cx+y,cy-x); setPixel(cx-y,cy-x);
+
+        y++;
+        if(p<=0) p+=2*y+1;
+        else { x--; p+=2*y-2*x+1; }
+    }
+}
+
+void drawTriangle(int x1,int y1,int x2,int y2,int x3,int y3) {
+    drawLine(x1,y1,x2,y2);
+    drawLine(x2,y2,x3,y3);
+    drawLine(x3,y3,x1,y1);
+}
